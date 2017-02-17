55e5785995693cb86f87fa64585f4a4aa4124de9
Ruslan Variushkin <ruslan@host4.biz>
2017-02-18 00:17:58 +0100

    initial commit

--- mgr5back.py
+++ mgr5back.py
@@ -2,7 +2,11 @@
 
 import sys
 import os
+import xmltodict
+import time
 
+NodeID='2'
+NoBackupID='151'
 ftp_conn='/usr/local/mgr5/etc/.vmmgr-backup/storages/st_1'
 pidfile = '/tmp/vdsback.pid'
 BackDir='/backup'
@@ -21,44 +25,15 @@ def Check():
         sys.exit()
     else:
         file(pidfile, 'w').write(pid)
+        
 
-def MysqlGet():
-    global VarMysql
+def Mysqlget(SQL):
     VarMysql={}
     St=['DBHost','DBUser','DBPassword','DBName']
     for line in open(FileDB,'r').readlines():
         parts = line.split() # split line into parts
         if len(parts) > 1: 
             VarMysql[parts[0]] = parts[1]
-
-def LvmBackup(Name, Size, Pool): 
-    import datetime
-    date=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    PoolName='/dev/'+Pool+"/"+Name
-    filez=BackDir+"/"+Name+"_"+date
-    NameImgFtp=Name+"_"+date
-    print "Start creating LVM Snapshote "+Name
-    cmdCreateLVM="lvcreate -L%sM -s -n %s-snapshot %s"%(Size,Name,PoolName )
-    #cmdRm0LVM="kpartx -d %s-snapshot"%(PoolName)
-    cmdRm1LVM="lvremove -f %s-snapshot"%(PoolName)
-    os.system(cmdCreateLVM)  # create LVM snapeshot
-    if Gzip is "YES":
-        cmdDD="dd if=%s-snapshot | gzip -c > %s "%(PoolName, filez)
-        os.system(cmdDD)  # start dd
-    else:
-        filez=PoolName
-    ftpput(filez,NameImgFtp)   # put to ftp
-    #os.system(cmdRm0LVM)  # remove LVM snapeshot by kpartx
-    os.system(cmdRm1LVM)  # remove LVM snapeshot
-    if Gzip is "YES":
-        rmf="rm %s"%(filez)
-        os.system(rmf)              # remove gzip file 
-    #print "#########",date,"##############"
-
-
-
-def Start():
-    MysqlGet()
     import mysql.connector
     from mysql.connector import errorcode
     try:
@@ -74,51 +49,99 @@ def Start():
             print(err)
     else:
         cur = cnx.cursor()
-        Res=cur.execute("select vm.name, vm.vsize, volume.pool from volume  join vm on vm.name =volume.name;")
+        cur.execute(SQL)
         Servs=list(cur.fetchall())
-        for R in Servs:
-            LvmBackup(R[0],R[1], R[2])
         cnx.close()
+    return Servs
+
+def Search():
+   sql="select id from vm where hostnode=\'%s\' and id not in (%s);"%(NodeID, NoBackupID)
+   Servs=Mysqlget(sql)
+   for R in Servs:
+       StartBackup(R[0])
+
+def StartBackup(ServerID):
+    #print ServerID
+    sql="select vm,name,pool,size from volume where vm=\'%s\' and hostnode=\'%s\' and pool is not NULL;"%(ServerID,NodeID)
+    Serv=Mysqlget(sql)
+    #print Serv
+    for R in Serv:
+        W=work(R[1], R[2])
+        W.CreateLVM(R[3])
+    for R in Serv:
+        W=work(R[1], R[2])
+        W.CreateGzip()
+        W.PutFtp()
 
 
-def ftpput(file, NameImg):
-    print "Start put file to backup server"+file
-    import xmltodict
-    with open(ftp_conn) as fd:
-        doc = xmltodict.parse(fd.read())
-        nameb=doc['doc']['name']
-        passftp=doc['doc']['settings']['password']
-        url=doc['doc']['settings']['url']
-        user=doc['doc']['settings']['username']
-    from ftplib import FTP
-    ftp = FTP(url)
-    ftp.login(user, passftp)
-    #print ftp.mkd(nameb)
-    if nameb in ftp.nlst('') :
-        pass
-    else :
-        print 'NO Dir, Start create'
-        ftp.mkd(nameb)
-    ftp.cwd(nameb)
-    ftp.storbinary("STOR %s"%(NameImg), open(file))
-    ftp.quit() 
-
-    
- # Need create LVM to file with date
- # Need clean file and clean ftp store 
- 
- 
+class work:
+    def __init__(self, Name, Pool):
+        import datetime
+        self.date=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        self.Name=Name
+        self.Pool=Pool
+        PoolName='/dev/'+Pool+"/"+Name
+        self.filez=BackDir+"/"+Name+"_"+self.date
+        NameImgFtp=Name+"_"+self.date
+        self.dftp=self.Name+"/"+self.date
+        #print PoolName
+    def CreateLVM(self, Size):
+     #   print "Name: ", self.Name," Size:  ", Size," Pool: "+ self.Pool
+        print "Start creating LVM Snapshote "+Name
+        cmdCreateLVM="lvcreate -L%sM -s -n %s-snapshot %s"%(Size,Name,PoolName )
+        os.system(cmdCreateLVM)
+    def CreateGzip(self):
+        print "Create gzip file, pool: "+self.Pool+" backup file: "+self.filez
+        cmdDD="dd if=%s-snapshot | gzip -c > %s "%(PoolName, filez)
+        os.system(cmdDD)  # start dd
+    def PutFtp(self):
+        print "Upload a file via FTP :"+self.filez
+        with open(ftp_conn) as fd:
+            doc = xmltodict.parse(fd.read())
+            nameb=doc['doc']['name']
+            passftp=doc['doc']['settings']['password']
+            url=doc['doc']['settings']['url']
+            user=doc['doc']['settings']['username']
+        import ftplib
+        from ftplib import FTP
+        print self.Name
+        DIR=NodeID+"/"+nameb+"/"+self.Name+"/"+self.date
+        ftp = FTP(url)
+        ftp.login(user, passftp)
+        #print ftp.mkd(nameb)
+        try:
+            ftp.cwd(NodeID)
+        except ftplib.error_perm:
+            ftp.mkd(NodeID)
+        try:
+            ftp.cwd(nameb)
+        except ftplib.error_perm:
+            ftp.mkd(nameb)
+        try:
+            ftp.cwd(self.Name)
+        except:
+            ftp.mkd(self.Name)
+        try:
+            ftp.cwd(self.date)
+        except ftplib.error_perm:
+            ftp.mkd(self.date)
+        ftp.cwd(DIR)
+        print "Upload to "+DIR
+        ftp.storbinary("STOR %s"%(self.NameImgFtp), open(self.filez))
+        ftp.quit()
+       
+      
+      
 def Main(): 
-    #Check()
-   # print "good"
-    #import time
-    #time.sleep(10)
-    #MysqlGet()
-    Start()
-    #print VarMysql['DBHost']
-    #os.remove(pidfile)
-    #ftpget()
-
+    if len(sys.argv) > 1:
+         Startbackup(sys.argv[1])
+    else:
+        Search()
+    #sql="select name,pool,size from volume where hostnode=\'%s\' and vm not in (%s) and pool is not NULL;"%(NodeID, NoBackupID)
+    #res=Mysqlget(sql)
+    #print res
+    
+    
 if __name__ == '__main__':
     Main()
 
