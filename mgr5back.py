5761462e10b1104c4237d02abee5fd117bed424e
Ruslan Variushkin <ruslan@host4.biz>
2017-04-27 12:31:13 +0200

    initiall commit

--- mgr5back.py
+++ mgr5back.py
@@ -437,7 +437,7 @@ def help():
     \tftpold     - Show old or excess directories in the Node ID directory of the ftp server
     \tftpdel     - Remove some file or directory on the FTP server
     \tclean      - Remove old or excess directories in the Node ID directory of the ftp server
-    \tcreate-zab  - Create all zabbix marks
+    \tcreate-zab - Create all zabbix marks
     \thelp       - Print help\n"""
 
 
